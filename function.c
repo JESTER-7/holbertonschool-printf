a37a457a665eb7c5df43a65e649b5fc6e0dc5659
JESTER-7 <arthur.urbano33@gmail.com>
2024-11-25 11:57:52 +0100

    start

--- function.c
+++ function.c
@@ -1,20 +1,49 @@
 #include "main.h"
 /**
- * 
+ * _printf - litteraly printf
+ * @format: 
  */
 int _printf(const char *format, ...)
 {
 	int i = 0;
+	int compteur = 0;
+	int result_function = 0;
 	while (format[i] != '\0')
 	{
-		if (format[i] == '%')
+		if (format[i] == '\\')
 		{
-			get_function(format[i + 1]);
+			i = i + 1;
+			if (format[i] == 'n')
+				_putchar('\n');
+			else if (format[i] == 'a')
+				_putchar('\a');
+			else if (format[i] == 'b')
+				_putchar('\b');
+			else if (format[i] == 'f')
+				_putchar('\f');
+			else if (format[i] == 'r')
+				_putchar('\r');
+			else if (format[i] == 't')
+				_putchar('\t');
+			else if (format[i] == 'v')
+				_putchar('\v');
+			else
+				_putchar(format[i]);
+			compteur++;
+		}
+		else if (format[i] == '%')
+		{
+			result_function = get_function(format[i + 1]);
+			compteur = result_function + compteur;
+			if (result_function == NULL)
+				exit(99);
 		}
 		else
 		{
 			_putchar(format[i]);
+			compteur++;
 		}
 		i++;
 	}
+	return (compteur);
 }
--- main.h
+++ main.h
@@ -5,9 +5,15 @@
 #include <stdarg.h>
 int _putchar(char c);
 int _printf(const char *format, ...);
+int percent(va_list);
+int integer(va_list);
+int integerten(va_list);
+int character(va_list);
+int string(va_list);
+
 typedef struct printf
 {
 	char *op;
-	int (*f)(va_arg);
+	int (*f)(va_list p);
 } print;
 #endif
